931349165eacf7b6bf9d3b0318bb64d974daf242
Daniel Han <daniel.han@scrive.com>
2020-02-26 14:06:04 +0100

    Add animation for copy button

--- src/components/two-factor-token-generator/index.js
+++ src/components/two-factor-token-generator/index.js
@@ -1,12 +1,13 @@
-import { authenticator } from "otplib";
+import { authenticator } from "otplib"
 import React, { useState } from 'react'
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonInput } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonInput } from '@ionic/react'
 import styles from './index.module.css'
 
 const TwoFactorTokenGenerator = (props) => {
   const [ secret, setSecret ] = useState(localStorage.getItem('TwoFactorTokenSecret') || null)
   const totpCode = secret ? authenticator.generate(secret) : ''
   const [ code, setCode ] = useState(totpCode)
+  const [ buttonText, setButtonText ] = useState('Copy')
 
   const updateSecret = (event) => {
     console.log('event happened')
@@ -24,6 +25,9 @@ const TwoFactorTokenGenerator = (props) => {
     el.select()
     document.execCommand('copy')
     document.body.removeChild(el)
+
+    setButtonText('Copied')
+    setTimeout(() => setButtonText('Copy'), 400)
   }
 
   setInterval(() => {
@@ -44,7 +48,7 @@ const TwoFactorTokenGenerator = (props) => {
         <IonInput className={styles.secret} value={secret} onIonChange={updateSecret} placeholder="Please fill in the secret here."/>
         <div className={styles.result}>
         <div>Token: {code} </div>
-        <IonButton onClick={() => copyToClipboard(code)}>Copy</IonButton>
+          <IonButton onClick={() => copyToClipboard(code)}>{buttonText}</IonButton>
         </div>
       </div>
     </IonCardContent>
