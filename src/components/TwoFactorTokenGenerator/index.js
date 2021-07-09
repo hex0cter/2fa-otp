ff3d1944bb0d068048da9820443838b9d31db929
Daniel Han <hex0cter@gmail.com>
2021-07-09 13:06:54 +0200

    Get rid of @ionic/react

--- src/components/TwoFactorTokenGenerator/index.js
+++ src/components/TwoFactorTokenGenerator/index.js
@@ -1,9 +1,8 @@
 import { authenticator } from "otplib"
 import React, { useState } from 'react'
-import { IonCardContent, IonCardTitle } from '@ionic/react'
 import { makeStyles } from '@material-ui/core/styles';
+import HourglassFullIcon from '@material-ui/icons/HourglassFull';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -42,61 +41,44 @@ const TwoFactorTokenGenerator = () => {
   }
 
   const generateCode = () => {
-    setRefreshButtonText('Refreshing')
+    setRefreshButtonText(<HourglassFullIcon />)
     const newTotpCode = secret ? authenticator.generate(secret) : ''
     if (newTotpCode !== code) {
       setCode(newTotpCode)
     }
-    setTimeout(() => setRefreshButtonText('Refresh'), 1500)
+    setTimeout(() => setRefreshButtonText('Refresh'), 100)
   }
 
-  window.addEventListener("focus", generateCode)
+  window.addEventListener("focus", generateCode);
 
-  const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-      color: "rgba(0, 0, 0, 0.54)",
+  const useStyles = makeStyles((theme) => ({
+    button: {
+      margin: theme.spacing(1),
     },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  });
-
+  }));
   const classes = useStyles();
 
   return (
-    <Card className={classes.root} variant="outlined" >
+    <Card variant="outlined" >
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-          Fill in your OTP secret to get the latest One time password. Your secret is never sent over the internet.
+          <Typography variant="subtitle2">
+          Fill in your OTP secret to get the latest One time password. <br/>
+          Your secret is only cached locally and never sent over the internet.
           </Typography>
         </Toolbar>
-        </AppBar>
-      <CardActions>
-        <IonCardTitle></IonCardTitle>
-      </CardActions>
+      </AppBar>
       <CardContent>
-        <IonCardContent>
-          <div className={styles.twoFactorContainer}>
-            <SecretField secret={secret} onChange={updateSecret} />
-            <div className={styles.result}>
-              <div>Token: {code} </div>
-              <div>
-                <Button variant="contained" color="primary" onClick={() => copyToClipboard(code)}>{copyButtonText}</Button> &nbsp;
-                <Button variant="contained" color="primary" onClick={generateCode}>{refreshButtonText}</Button>
-              </div>
+        <div className={styles.twoFactorContainer}>
+          <SecretField secret={secret} onChange={updateSecret} />
+          <div className={styles.result}>
+            <div>Token: {code} </div>
+            <div>
+              <Button variant="contained" color="primary" onClick={() => copyToClipboard(code)} className={classes.button}>{copyButtonText}</Button>
+              <Button variant="contained" color="default" onClick={generateCode}>{refreshButtonText}</Button>
             </div>
           </div>
-        </IonCardContent>
+        </div>
       </CardContent>
     </Card>
 
