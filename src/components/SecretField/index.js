a4bbe34c9403a4e98117ab1f7aff8b712df14506
Daniel Han <hex0cter@gmail.com>
2021-07-09 11:24:09 +0200

    Introduce material design

---
+++ src/components/SecretField/index.js
@@ -0,0 +1,49 @@
+
+import * as React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+
+export default function SecretInput(props) {
+  const [values, setValues] = React.useState({
+    showPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  return (
+      <div>
+        <FormControl sx={{ m: 1, width: '50%' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">OTP Secret</InputLabel>
+          <Input
+            id="outlined-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={props.secret}
+            onChange={props.onChange}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+      </div>
+  );
+}
--- src/components/two-factor-token-generator/index.js
+++ src/components/two-factor-token-generator/index.js
@@ -1,7 +1,8 @@
 import { authenticator } from "otplib"
 import React, { useState } from 'react'
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonInput } from '@ionic/react'
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react'
 import styles from './index.module.css'
+import SecretField from '../SecretField'
 
 const TwoFactorTokenGenerator = () => {
   const [ secret, setSecret ] = useState(localStorage.getItem('TwoFactorTokenSecret') || null)
@@ -45,11 +46,11 @@ const TwoFactorTokenGenerator = () => {
   return (
     <IonCard>
     <IonCardHeader>
-      <IonCardTitle>TOTP Secret</IonCardTitle>
+      <IonCardTitle>Fill in your OTP secret to get the latest One time password. Your secret is never sent over the internet.</IonCardTitle>
     </IonCardHeader>
     <IonCardContent>
       <div className={styles.twoFactorContainer}>
-        <IonInput className={styles.secret} value={secret} onIonChange={updateSecret} placeholder="Please fill in the secret here."/>
+        <SecretField secret={secret} onChange={updateSecret} />
         <div className={styles.result}>
         <div>Token: {code} </div>
           <IonButton onClick={() => copyToClipboard(code)}>{copyButtonText}</IonButton>
